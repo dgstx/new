97d07a30ff50a538f5431eeb3543f548cca103fe
dgstx <dgsomacal@gmail.com>
2023-03-07 22:45:25 -0300

    ok

---
+++ frontend/src/routes/Route.js
@@ -0,0 +1,38 @@
+import React, { useContext } from "react";
+import { Route as RouterRoute, Redirect } from "react-router-dom";
+
+import { AuthContext } from "../context/Auth/AuthContext";
+import BackdropLoading from "../components/BackdropLoading";
+
+const Route = ({ component: Component, isPrivate = false, ...rest }) => {
+	const { isAuth, loading } = useContext(AuthContext);
+
+	if (!isAuth && isPrivate) {
+		return (
+			<>
+				{loading && <BackdropLoading />}
+				//<Redirect to={{ pathname: "/login", state: { from: rest.location } }} />
+				<Redirect to={{ pathname: "/home", state: { from: rest.location } }} />
+
+			</>
+		);
+	}
+
+	if (isAuth && !isPrivate) {
+		return (
+			<>
+				{loading && <BackdropLoading />}
+				<Redirect to={{ pathname: "/", state: { from: rest.location } }} />;
+			</>
+		);
+	}
+
+	return (
+		<>
+			{loading && <BackdropLoading />}
+			<RouterRoute {...rest} component={Component} />
+		</>
+	);
+};
+
+export default Route;
